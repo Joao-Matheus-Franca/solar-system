143ca4757cef8bc0462f66303c06b50bcfff3b78
João Matheus Silva Franca <matheussfranca@live.com>
2022-08-19 10:25:11 -0300

    Ajustes nos Requisitos 03 e 04

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Title extends React.Component {
   render() {
-    const headline = 'planets Planetas';
+    const { headline } = this.props;
     return (
       <h2>{headline}</h2>
     );
   }
 }
 
+Title.propTypes = {
+  headline: PropTypes.string.isRequired,
+};
 export default Title;
